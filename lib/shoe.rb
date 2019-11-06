49d5a8b993211e2ca026edb50a3874b277ae86f0
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-06 19:34:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,8 @@ class Shoe
   end 
   
   def cobble
-        puts "Your shoe is as good as new!"
-        @condition = "new"
-    end
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
   
 end 
